753dcbd63066e42e1d4611cad60eebda996aea79
wong hejun <wonghejun@outlook.com>
2020-05-04 19:10:40 +0800

    Add project files.

---
+++ AsyncAwait/FilePolling.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class FilePolling
+    {
+        public async Task CheckFileIDAsync(string file_directory, string keyword)
+        {
+            Console.WriteLine("Enter CheckFileID method");
+
+            string fileid = await Task.Run(() => GetFileIDWithKeyword(file_directory, keyword));
+
+            Console.WriteLine("this is executed after await task");
+        }
+
+        private string GetFileIDWithKeyword(string file_directory, string keyword)
+        {
+            Console.WriteLine("Enter GetFileNameWithKeyword method");
+
+            string selectedfilename = string.Empty;
+            string fileid = "error";
+            
+            for(int iteration_count = 1; iteration_count < 5; iteration_count++) 
+            {
+                //go to the path and all files
+                Console.WriteLine(selectedfilename);
+                Console.WriteLine("iteration" + iteration_count.ToString());
+                string[] fileEntries = Directory.GetFiles(file_directory);
+
+                foreach (string filename in fileEntries)
+                {
+                    if (filename.Contains(keyword))
+                    {
+                        selectedfilename = filename;
+                        break;
+                    }
+                }
+
+                if (selectedfilename != string.Empty || iteration_count == 4)
+                    break;
+
+                Console.WriteLine("about to apply delay");
+                Task.Delay(30000).Wait();
+                Console.WriteLine("delay applied");
+                Console.WriteLine(iteration_count.ToString());
+            }
+
+            Console.WriteLine(selectedfilename);
+
+            if (selectedfilename != string.Empty)
+            {
+                //split out the fileid keyword-fileid-xxxx.pdf
+                string[] fileInfoArray = selectedfilename.Split('-');
+                fileid = fileInfoArray[1].ToString();
+            }
+
+            Console.WriteLine("Fileid is " + fileid);
+            Console.WriteLine("Exit GetFileNameWithKeyword method");
+        
+            return fileid;
+        }
+    }
+}
---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static string fileDirectory = @"C:\Users\wongh\Documents\test";
+
+        static TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            FilePolling poll = new FilePolling();
+
+            poll.CheckFileIDAsync(fileDirectory, "hello");
+            
+            Console.WriteLine("This is outside of async codes");
+
+            Console.ReadKey();
+        }
+
+    }
+}
